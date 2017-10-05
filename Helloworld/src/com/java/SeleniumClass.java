2c2e60c71a5700634ecd0f898cba090f8895505d
rkiran323 <ravikiran323@gmail.com>
2017-10-04 22:30:18 -0400

    Added new print statement

--- Helloworld/src/com/java/SeleniumClass.java
+++ Helloworld/src/com/java/SeleniumClass.java
@@ -6,7 +6,7 @@ public class SeleniumClass
 	public static void main( String[] args)
 	{
 		System.out.println("This is my first selenium class");
-
+System.out.println("We are testing Git Hub");
 	}
 
 }
